0c8c2eb55ca3190c4527789ad4447689ed5e593d
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-01-10 16:19:48 -0800

    added knexfile and app.js server contents

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+if(process.env.NODE_ENV !== 'production') require('dotenv').load()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/users', require('./routes/user-routes'))
+app.use('/auth', require('./routes/auth-routes'))
+// SETUP app.get here
+
+app.use((req, res, next) => {
+    next({status:404, message: "Unable to locate"})
+})
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    const status =  err.status || 500
+    res.status(status).send(err)
+})
+
+const listener = () => console.log(`Listening on ${port}`)
+app.listen(port, listener)
