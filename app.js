25b7fcda69a0c6db9857e69399dcb142b0557b2f
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-01-14 15:21:14 -0800

    committing to equalize, minor change in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').load()
 
